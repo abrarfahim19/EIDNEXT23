838380baab96dbbcafbf16f29bf950afb42e27c0
abrarfahim19 <fahimpatwari19@gmail.com>
2023-04-25 13:53:50 +0600

    analytics

--- FastBlog/nextjs-blog/pages/_app.js
+++ FastBlog/nextjs-blog/pages/_app.js
@@ -1,7 +1,11 @@
 import "../styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const App = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return <>
+        <Component {...pageProps} />
+        <Analytics />
+    </>
 }
 
 export default App;
